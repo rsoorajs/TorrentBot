9c98d92a09614c2f1b306b96ea59bd640528d720
paduhq <63902328+paduhq@users.noreply.github.com>
2020-04-20 17:13:02 +0800

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  TOKEN: '1286381136:AAG162UVr5963UqYpuwcEWVNNRheevZvRfs',
+  TOKEN: '1243705125:AAFNuxqgjvMIHWIh4-aS-rnDDn25-fPAsHc',
   ARIA_SECRET: 'hasdhashdasndashdagnausdasmd',
   ARIA_DOWNLOAD_LOCATION: '/content',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
@@ -7,8 +7,8 @@ module.exports = Object.freeze({
   ARIA_FILTERED_FILENAMES: ['TIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
   GDRIVE_PARENT_DIR_ID: '1-1YFGE3OEv4rPyM4G16jeACTxJNeBHuK',
-  SUDO_USERS: [254210865, 1184227743],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [254210865,1184227743],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  SUDO_USERS: [254210865, 1184227743, 556218472],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [254210865,1184227743, -1001345000208],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
