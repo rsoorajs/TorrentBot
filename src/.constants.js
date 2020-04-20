164ee8685999e5cf747e446ab222f7960061127f
paduhq <63902328+paduhq@users.noreply.github.com>
2020-04-20 22:13:27 +0800

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -6,7 +6,7 @@ module.exports = Object.freeze({
   ARIA_FILTERED_DOMAINS: ['jizz', 'porn', 'cruzing.xyz', 'eztv.ap', 'porab'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['TIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: '1-1YFGE3OEv4rPyM4G16jeACTxJNeBHuK',
+  GDRIVE_PARENT_DIR_ID: '1-0cmAPC9Qvsz0uR1aNqUsMyhwXsuwMPa',
   SUDO_USERS: [254210865, 1184227743, 556218472],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [254210865,1184227743, -1001345000208],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
