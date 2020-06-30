4ac5483f9a1a3505bc51bef7a22a29c896c685d6
paduhq <63902328+paduhq@users.noreply.github.com>
2020-06-30 19:25:42 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -3,7 +3,7 @@
 ## Set the variable below to your Aria password
 ARIA_RPC_SECRET="hasdhashdasndashdagnausdasmd"
 ## This is the maximum number of download jobs that will be active at a time. Note that this does not affect the number of concurrent *uploads*
-MAX_CONCURRENT_DOWNLOADS=3
+MAX_CONCURRENT_DOWNLOADS=12
 ## The port that RPC will listen on
 RPC_LISTEN_PORT=8210
 aria2c --bt-tracker=https://raw.githubusercontent.com/cikgusuraya/KillerAdBlok/master/tracker.txt --bt-max-peers=0  --enable-rpc --rpc-listen-all=false --rpc-listen-port $RPC_LISTEN_PORT --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --rpc-secret=$ARIA_RPC_SECRET --max-overall-upload-limit=1  --daemon=true
